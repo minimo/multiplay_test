8390e09d28ba3cd7897cf7f8c308d711886b204a
minimo <minimo.mobile@gmail.com>
2016-03-09 19:07:01 +0900

    add firebase accessory

---
+++ src/firebeselinker.js
@@ -0,0 +1,47 @@
+/*
+ *  FireBaseLinker.js
+ *  2014/09/05
+ *  @auther minimo  
+ *  This Program is MIT license.
+ */
+
+
+phina.define("multi.FireBaseReceiver", {
+    superClass: "phina.accessory.Accessory",
+
+    init: function(firebase) {
+        this.superInit();
+        this.firebase = firebase;
+
+        //firebaseと情報を同期
+        this.firebase.on('child_changed', function(snap) {
+            if (snap.key() === this.key) {
+                var v = snap.val();
+                this.target.x = v.x;
+                this.target.y = v.y;
+            }
+        }.bind(this));
+    },
+});
+
+phina.define("multi.FireBaseSender", {
+    superClass: "phina.accessory.Accessory",
+
+    data: null,
+
+    init: function(firebase) {
+        this.superInit();
+        this.firebase = firebase;
+    },
+
+    update: function() {
+        this.firebase.update({
+            x: this.target.x,
+            y: this.target.y,
+        });
+    },
+
+    setSendData: function(data) {
+        this.data = data || {};
+    },
+});
